4b8d45acc9af4cb1dc544f446e6f9a536c80b743
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2024-02-28 16:04:53 +0200

    Server UI

--- NPLab1/Server.UI/Program.cs
+++ NPLab1/Server.UI/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Server.UI;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Server";
+        ServerApp app = new ServerApp();
+        app.Main();
+    }
+}
---
+++ NPLab1/Server.UI/ServerApp.cs
@@ -0,0 +1,50 @@
+﻿using Server.BL;
+
+namespace Server.UI;
+
+public class ServerApp : IServerLogger
+{
+    bool go = true;
+
+    public void Main()
+    {
+        ServerBL serverBl = new(this);
+        while (go)
+        {
+            Console.WriteLine("Server UI\n\tInput format:\n\t\t <ServerIP/Any/127.0.0.1>:<ServerPort>\n");
+            try
+            {
+                var data = Console.ReadLine().Split(":");
+                serverBl = new(this, data[0], int.Parse(data[1]));
+                go = false;
+            }
+            catch
+            {
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        serverBl.StartServer();
+        go = true;
+        Console.WriteLine("Server UI\n\tInput \"\\Cancel\" for exiting");
+        while (go)
+        {
+            var command = Console.ReadLine();
+            if (command == "\\Cancel")
+            {
+                serverBl.StopServer();
+                go = false;
+            }
+        }
+
+    }
+
+    public void Log(string message)
+    {
+        lock (Console.Out)
+        {
+            Console.WriteLine($"{message}\n");
+        }
+    }
+}
